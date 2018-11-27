1e607219bdf444a02f95cf859a9e0a688adaccc4
JaneWay <nkuborn@gmail.com>
2018-11-27 23:19:31 +0100

    created structure for the local weather raspberry project

---
+++ fetch_weather.py
@@ -0,0 +1,11 @@
+
+from requests import get
+import json
+from pprint import pprint
+
+url = 'https://apex.oracle.com/pls/apex/raspberrypi/weatherstation/getlatestmeasurements/2828199'
+
+weather = get(url).json()['items']
+pprint(weather)
+
+
